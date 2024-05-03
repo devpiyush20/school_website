76adbaf64d40c2ac46e269fd9c674b3ba3546e61
Vimal Mishra <skk180509@gmail.com>
2024-05-03 16:06:32 +0530

    nsdcbxnz

--- components/ImageCarousel.js
+++ components/ImageCarousel.js
@@ -10,11 +10,11 @@ const ImageCarousel=()=> {
     const [currentSlide, setCurrentSlide] = useState(0);
 
     const handlePrevSlide = () => {
-        setCurrentSlide(prevSlide => (prevSlide === 0 ? 4 : prevSlide - 1));
+        setCurrentSlide(prevSlide => (prevSlide === 0 ? 3 : prevSlide - 1));
     };
 
     const handleNextSlide = () => {
-        setCurrentSlide(prevSlide => (prevSlide === 4 ? 0 : prevSlide + 1));
+        setCurrentSlide(prevSlide => (prevSlide === 3 ? 0 : prevSlide + 1));
     };
 
     return (
@@ -23,18 +23,18 @@ const ImageCarousel=()=> {
             <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
                 {images.map((item, index) => (
                     <div key={index} className={`duration-700 ease-in-out ${index === currentSlide ? '' : 'hidden'}`} data-carousel-item>
-                        <img src={item} className="absolute block lg:mt-10 w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt={`Slide ${index+1}`} />
+                        <img src={item} className="absolute z-1 block lg:mt-10 w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt={`Slide ${index+1}`} />
                     </div>
                 ))}
             </div>
             {/* Slider indicators */}
-            <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-                {[0, 1, 2, 3, 4].map(index => (
-                    <button key={index} type="button" className={`w-3 h-3 rounded-full ${index === currentSlide ? 'bg-black' : 'bg-gray-300'}`} aria-current={index === currentSlide} aria-label={`Slide ${index + 1}`} onClick={() => setCurrentSlide(index)}></button>
+            <div className="absolute z-3 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+                {[0, 1, 2, 3].map(index => (
+                    <button key={index} type="button" className={`w-3 h-3   rounded-full ${index === currentSlide ? 'bg-black' : 'bg-gray-300'}`} aria-current={index === currentSlide} aria-label={`Slide ${index + 1}`} onClick={() => setCurrentSlide(index)}></button>
                 ))}
             </div>
             {/* Slider controls */}
-            <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev onClick={handlePrevSlide}>
+            <button type="button" className="absolute z-3 top-0 start-0  flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev onClick={handlePrevSlide}>
                 <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-white-800/30 group-hover:bg-white/50 dark:group-hover:bg-white-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-white-800/70 group-focus:outline-none">
                     <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
@@ -42,7 +42,7 @@ const ImageCarousel=()=> {
                     <span className="sr-only">Previous</span>
                 </span>
             </button>
-            <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next onClick={handleNextSlide}>
+            <button type="button" className="absolute z-3 top-0 end-0  flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next onClick={handleNextSlide}>
                 <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-white-800/30 group-hover:bg-white/50 dark:group-hover:bg-white-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-white-800/70 group-focus:outline-none">
                     <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
